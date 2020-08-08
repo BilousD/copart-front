d55d075f8b5e6b04260c243cfdd84b98d06cadbd
BilousD <dibilous1@gmail.com>
2020-08-08 16:11:49 +0300

    proxy fix

--- proxy.js
+++ proxy.js
@@ -1,7 +1,7 @@
 var HttpsProxyAgent = require('https-proxy-agent');
 var proxyConfig = [{
     context: '/api',
-    target: 'http://localhost:8080',
+    target: 'http://localhost:3000',
     secure: false
 }];
 
