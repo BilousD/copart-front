60de0a59d68e0b84a8568383d0a0f7c1448eb9ff
BilousD <dibilous1@gmail.com>
2020-08-18 21:03:41 +0300

    options txt (shit, needs rework) should work 25/8 (i hope)

--- src/app/lot-info.component.ts
+++ src/app/lot-info.component.ts
@@ -22,16 +22,16 @@ export class LotInfoComponent implements OnInit {
 
     fuelToExciseExample = {
         // fuel type (as specified in copart)
-        'DIESEL':{
+        'DIESEL':[
             // higher breakpoint: excise rate; 0 - highest breakpoint? or something like that
-            3.5:75,
-            30:150,
-            0:300
-        },
-        'GAS':{
-            3:50,
-            0:100
-        },
+            [3.5, 75],
+            [30, 150],
+            [0, 300]
+        ],
+        'GAS':[
+            [3, 50],
+            [0, 100]
+        ],
         // if DEFAULT specified, it is used if fuel type is not specified, or no excise rate specified for this fuel type
         'DEFAULT':50
     };
@@ -41,41 +41,41 @@ export class LotInfoComponent implements OnInit {
         'OK':1500,
         'NV':1450
     };
-    auctionFees = {
+    auctionFees = [
         // higher breakpoint: fee
-        50:27.5,
-        100:40,
-        200:65,
-        300:90,
-        400:110,
-        500:130,
-        600:150,
-        700:170,
-        800:190,
-        900:210,
-        1000:230,
-        1200:255,
-        1300:290,
-        1400:305,
-        1500:315,
-        1600:325,
-        1700:345,
-        1800:355,
-        2000:375,
-        2400:415,
-        2500:425,
-        3000:455,
-        3500:565,
-        4000:615,
-        4500:640,
-        5000:665,
-        6000:765,
-        7500:790,
-        10000:890,
-        15000:1050,
+        [50,27.5],
+        [100,40],
+        [200,65],
+        [300,90],
+        [400,110],
+        [500,130],
+        [600,150],
+        [700,170],
+        [800,190],
+        [900,210],
+        [1000,230],
+        [1200,255],
+        [1300,290],
+        [1400,305],
+        [1500,315],
+        [1600,325],
+        [1700,345],
+        [1800,355],
+        [2000,375],
+        [2400,415],
+        [2500,425],
+        [3000,455],
+        [3500,565],
+        [4000,615],
+        [4500,640],
+        [5000,665],
+        [6000,765],
+        [7500,790],
+        [10000,890],
+        [15000,1050],
         // if less than 1, than it's a %
-        0:0.10
-    };
+        [0,0.10]
+    ];
 
     bigScreen = false;
 
@@ -104,30 +104,26 @@ export class LotInfoComponent implements OnInit {
 
     calculate() {
         let exciseRateEur = this.fuelToExciseExample['DEFAULT'];
-        let volume = 2.5;
-        let year = 2020;
-        let price = 0;
-        let euroUsd = 1.18;
+        let volume = parseFloat(this.data.engine.split('L')[0]);
+        let year = parseInt(this.data.year);
+        let price = parseInt((this.carPrice)?this.carPrice:this.data.lot.currentBid);
+        let euroUsd = parseFloat(this.euro2usd);
         let aucFee = this.auctionFees['0'];
-        // try catch?
-        volume = parseFloat(this.data.engine.split('L')[0]);
-        year = parseInt(this.data.year);
-        price = parseInt((this.carPrice)?this.carPrice:this.data.lot.currentBid);
-        euroUsd = parseFloat(this.euro2usd);
-        //
+
         Object.keys(this.fuelToExciseExample).forEach(key => {
             if (key !== 'DEFAULT' && this.data.fuel === key) {
-                exciseRateEur = parseFloat(this.fuelToExciseExample[key]['0']);
-                Object.keys(this.fuelToExciseExample[key]).forEach(exciseRateKey => {
-                    if (volume < parseFloat(exciseRateKey)) {
-                        exciseRateEur = parseFloat(this.fuelToExciseExample[key][exciseRateKey]);
+                let array = this.fuelToExciseExample[key];
+                exciseRateEur = array[array.length-1][1];
+                for (let arr of array) {
+                    if (volume < arr[0]) {
+                        exciseRateEur = arr[1];
                     }
-                });
+                }
             }
         });
-        for(let key of Object.keys(this.auctionFees)) {
-            if (price < parseFloat(key)) {
-                aucFee = this.auctionFees[key];
+        for (let array of this.auctionFees) {
+            if (price < array[0]) {
+                aucFee = array[1];
                 break;
             }
         }
